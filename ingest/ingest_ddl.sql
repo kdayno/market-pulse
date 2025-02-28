fa4b3f664491e924082aa5c9704b2543eabca773
kdayno@gmail.com <kdayno@gmail.com>
2025-02-28 06:22:40 +0000

    Create Reddit posts data ingestion pipeline

--- ingest/ingest_ddl.sql
+++ ingest/ingest_ddl.sql
@@ -24,3 +24,50 @@ TBLPROPERTIES (
   'delta.autoOptimize.optimizeWrite' = 'true',
   'delta.autoOptimize.autoCompact' = 'true'
 );
+
+
+CREATE TABLE tabular.dataexpert.kdayno_bronze_reddit_top_posts (
+  ticker_symbol STRING,
+  post_id STRING,
+  post_title STRING,
+  subreddit_id STRING,
+  subreddit STRING,
+  created_utc DATE,
+  score INT,
+  upvote_ratio FLOAT,
+  num_comments INT,
+  post_body_text STRING,
+  is_self_post BOOLEAN,
+  is_original_content BOOLEAN,
+  permalink STRING,
+  post_url STRING,
+  load_date_ts TIMESTAMP
+  )
+USING delta
+TBLPROPERTIES (
+  'delta.autoOptimize.optimizeWrite' = 'true',
+  'delta.autoOptimize.autoCompact' = 'true'
+);
+
+CREATE TABLE tabular.dataexpert.kdayno_bronze_reddit_hot_posts (
+  ticker_symbol STRING,
+  post_id STRING,
+  post_title STRING,
+  subreddit_id STRING,
+  subreddit STRING,
+  created_utc DATE,
+  score INT,
+  upvote_ratio FLOAT,
+  num_comments INT,
+  post_body_text STRING,
+  is_self_post BOOLEAN,
+  is_original_content BOOLEAN,
+  permalink STRING,
+  post_url STRING,
+  load_date_ts TIMESTAMP
+  )
+USING delta
+TBLPROPERTIES (
+  'delta.autoOptimize.optimizeWrite' = 'true',
+  'delta.autoOptimize.autoCompact' = 'true'
+);
