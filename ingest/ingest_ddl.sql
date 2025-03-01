8ddf3cceda3079b3b7f6f84df54c6552e4adcc4d
kdayno@gmail.com <kdayno@gmail.com>
2025-03-01 01:11:23 +0000

    Update DDLs

--- ingest/ingest_ddl.sql
+++ ingest/ingest_ddl.sql
@@ -1,4 +1,4 @@
-CREATE TABLE tabular.dataexpert.kdayno_ingest_SP500Companies (
+CREATE TABLE tabular.dataexpert.kdayno_bronze_SP500_companies (
   Symbol STRING,
   Security STRING,
   GICS_Sector STRING,
@@ -10,7 +10,7 @@ CREATE TABLE tabular.dataexpert.kdayno_ingest_SP500Companies (
   load_date_ts TIMESTAMP)
 USING delta;
 
-CREATE TABLE tabular.dataexpert.kdayno_ingest_SP500_stock_prices (
+CREATE TABLE tabular.dataexpert.kdayno_bronze_SP500_stock_prices (
   ticker_symbol STRING,
   open_price DECIMAL(10,2),
   close_price DECIMAL(10,2),
@@ -73,3 +73,57 @@ TBLPROPERTIES (
   'delta.autoOptimize.optimizeWrite' = 'true',
   'delta.autoOptimize.autoCompact' = 'true'
 );
+
+CREATE TABLE tabular.dataexpert.kdayno_silver_reddit_top_posts_sentiment (
+  ticker_symbol STRING,
+  post_id STRING,
+  post_title STRING,
+  subreddit_id STRING,
+  subreddit STRING,
+  created_utc DATE,
+  score INT,
+  upvote_ratio FLOAT,
+  num_comments INT,
+  post_body_text STRING,
+  is_self_post BOOLEAN,
+  is_original_content BOOLEAN,
+  permalink STRING,
+  post_url STRING,
+  load_date_ts TIMESTAMP,
+  post_title_sentiment_result STRUCT<sentiment: STRING, sentiment_score: FLOAT>,
+  sentiment STRING,
+  sentiment_score FLOAT)
+USING delta
+PARTITIONED BY (subreddit)
+TBLPROPERTIES (
+  'delta.autoOptimize.optimizeWrite' = 'true',
+  'delta.autoOptimize.autoCompact' = 'true'
+);
+
+
+CREATE TABLE tabular.dataexpert.kdayno_silver_reddit_hot_posts_sentiment (
+  ticker_symbol STRING,
+  post_id STRING,
+  post_title STRING,
+  subreddit_id STRING,
+  subreddit STRING,
+  created_utc DATE,
+  score INT,
+  upvote_ratio FLOAT,
+  num_comments INT,
+  post_body_text STRING,
+  is_self_post BOOLEAN,
+  is_original_content BOOLEAN,
+  permalink STRING,
+  post_url STRING,
+  load_date_ts TIMESTAMP,
+  post_title_sentiment_result STRUCT<sentiment: STRING, sentiment_score: FLOAT>,
+  sentiment STRING,
+  sentiment_score FLOAT)
+USING delta
+PARTITIONED BY (subreddit)
+TBLPROPERTIES (
+  'delta.autoOptimize.optimizeWrite' = 'true',
+  'delta.autoOptimize.autoCompact' = 'true'
+);
+
